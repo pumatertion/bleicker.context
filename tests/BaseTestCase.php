6fca02c326110bc3b1391f236a3102b7d1986a2d
Carsten Bleicker <carsten@bleicker.de>
2015-05-17 16:11:27 +0200

    Introduce Context

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests\Bleicker\Context;
+
+/**
+ * Class BaseTestCase
+ *
+ * @package Tests\Bleicker\Context
+ */
+abstract class BaseTestCase extends \PHPUnit_Framework_TestCase {
+
+}
